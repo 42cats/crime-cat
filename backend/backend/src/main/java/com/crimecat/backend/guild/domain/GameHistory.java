bec130a31b6b0c65467b583ecb3aa75c278762c6
chyo1 <hyowon3856@gmail.com>
2025-03-14 20:10:10 +0900

    FEAT : GameHistory 엔티티 생성

---
+++ backend/backend/src/main/java/com/crimecat/backend/guild/domain/GameHistory.java
@@ -0,0 +1,47 @@
+package com.crimecat.backend.guild.domain;
+
+import com.crimecat.backend.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "GAME_HISTORY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class GameHistory {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "ID", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "IS_WIN", nullable = false)
+    private boolean isWin;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "CHARACTER_NAME", nullable = false)
+    private String characterName;
+
+    @JoinColumn(name = "USER_SNOWFLAKE", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @JoinColumn(name = "GUILD_SNOWFLAKE", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Guild guild;
+}
