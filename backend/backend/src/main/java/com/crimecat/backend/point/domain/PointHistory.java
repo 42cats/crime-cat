6b6b017417cec7e433152e3b7f0c0d687548555b
chyo1 <hyowon3856@gmail.com>
2025-03-14 19:21:55 +0900

    FEAT : PointHistory 엔티티 생성

---
+++ backend/backend/src/main/java/com/crimecat/backend/point/domain/PointHistory.java
@@ -0,0 +1,46 @@
+package com.crimecat.backend.point.domain;
+
+import com.crimecat.backend.permission.domain.Permission;
+import com.crimecat.backend.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "POINT_HISTORY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class PointHistory {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "ID", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @JoinColumn(name = "USER_SNOWFLAKE", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @JoinColumn(name = "PERMISSION_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Permission permission;
+
+    @Column(name = "POINT", nullable = false)
+    private Integer point;
+
+    @Column(name = "USED_AT", nullable = false)
+    private LocalDateTime usedAt;
+
+}
