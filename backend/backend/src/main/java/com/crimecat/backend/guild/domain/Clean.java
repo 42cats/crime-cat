971ba011b1ae25ab0998ec74a71b2e90b59f553a
chyo1 <hyowon3856@gmail.com>
2025-03-14 20:18:14 +0900

    FEAT : Clean 엔티티 생성

---
+++ backend/backend/src/main/java/com/crimecat/backend/guild/domain/Clean.java
@@ -0,0 +1,34 @@
+package com.crimecat.backend.guild.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "CLEAN")
+@NoArgsConstructor
+@Getter
+public class Clean {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "ID", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "CHANNEL_SNOWFLAKE", nullable = false)
+    private String channelSnowflake;
+
+    @JoinColumn(name = "GUILD_SNOWFLAKE", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Guild guild;
+}
