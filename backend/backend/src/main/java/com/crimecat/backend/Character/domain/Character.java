d52623820572c55160ebeb517687f75865db15e7
chyo1 <hyowon3856@gmail.com>
2025-03-23 23:32:46 +0900

    [BE] REFACTOR: 캐릭터 관련 도메인 패키지 분리

---
+++ backend/backend/src/main/java/com/crimecat/backend/Character/domain/Character.java
@@ -0,0 +1,37 @@
+package com.crimecat.backend.Character.domain;
+
+import com.crimecat.backend.guild.domain.Guild;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "CHARACTERS")
+@NoArgsConstructor
+@Getter
+public class Character {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "ID", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @JoinColumn(name = "GUILD_SNOWFLAKE", referencedColumnName = "SNOWFLAKE", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Guild guild;
+}
--- backend/backend/src/main/java/com/crimecat/backend/Character/domain/CharacterRole.java
+++ backend/backend/src/main/java/com/crimecat/backend/Character/domain/CharacterRole.java
@@ -1,38 +1,33 @@
-package com.crimecat.backend.guild.domain;
+package com.crimecat.backend.Character.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
-@Table(name = "CHARACTERS")
-@NoArgsConstructor
+@Table(name = "CHARACTER_ROLES")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Character {
+public class CharacterRole {
 
     @Id
     @UuidGenerator
     @Column(name = "ID", columnDefinition = "BINARY(16)")
     private UUID id;
 
-    @Column(name = "NAME", nullable = false)
-    private String name;
-
-    @Column(name = "CREATED_AT", nullable = false)
-    private LocalDateTime createdAt;
-
-    @JoinColumn(name = "GUILD_SNOWFLAKE", referencedColumnName = "SNOWFLAKE", nullable = false)
+    @JoinColumn(name = "CHARACTER", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
-    private Guild guild;
+    private Character character;
+
+    @Column(name = "ROLE_SNOWFLAKE", nullable = false)
+    private String roleSnowflake;
 }
