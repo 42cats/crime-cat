ca34e46543186e634e37f5b04666ac686b21462e
chyo1 <hyowon3856@gmail.com>
2025-03-27 16:39:53 +0900

    [BE] REFACTOR: GameHistory - updatable = false 설정

--- backend/backend/src/main/java/com/crimecat/backend/gameHistory/domain/GameHistory.java
+++ backend/backend/src/main/java/com/crimecat/backend/gameHistory/domain/GameHistory.java
@@ -24,23 +24,23 @@ public class GameHistory {
 
     @Id
     @UuidGenerator
-    @Column(name = "ID", columnDefinition = "BINARY(16)")
+    @Column(name = "ID", columnDefinition = "BINARY(16)", updatable = false)
     private UUID id;
 
-    @Column(name = "IS_WIN")
+    @Column(name = "IS_WIN", updatable = false)
     private boolean isWin = false;
 
-    @Column(name = "CREATED_AT", nullable = false)
+    @Column(name = "CREATED_AT", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "CHARACTER_NAME", nullable = false)
+    @Column(name = "CHARACTER_NAME", nullable = false, updatable = false)
     private String characterName;
 
-    @JoinColumn(name = "USER_SNOWFLAKE", referencedColumnName = "SNOWFLAKE", nullable = false)
+    @JoinColumn(name = "USER_SNOWFLAKE", referencedColumnName = "SNOWFLAKE", nullable = false, updatable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    @JoinColumn(name = "GUILD_SNOWFLAKE", referencedColumnName = "SNOWFLAKE", nullable = false)
+    @JoinColumn(name = "GUILD_SNOWFLAKE", referencedColumnName = "SNOWFLAKE", nullable = false, updatable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Guild guild;
 
