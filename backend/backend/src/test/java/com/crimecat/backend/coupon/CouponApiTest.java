b2d5ec5ad77c830b30224c4b0e2eff6ae46d768e
sabyun <hoone0802@gmail.com>
2025-04-01 15:30:23 +0900

    [BE]TEST: coupon TEST 추가수정 nullbody test and null check exception added

--- backend/backend/src/test/java/com/crimecat/backend/coupon/CouponApiTest.java
+++ backend/backend/src/test/java/com/crimecat/backend/coupon/CouponApiTest.java
@@ -46,7 +46,7 @@ public class CouponApiTest {
     }
 
     @Test
-    void 쿠폰_등록_성공() throws Exception {
+    void 쿠폰_등록_성공_실패() throws Exception {
         // 준비: 유저 & 쿠폰 저장
         String snowflake = String.valueOf(ThreadLocalRandom.current().nextInt(10000000, 100000000));
         User user = userRepository.save(User.of(snowflake,"test","url"));
@@ -60,6 +60,11 @@ public class CouponApiTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("Coupon redeemed successfully"))
                 .andExpect(jsonPath("$.point").value(5000));
+        mockMvc.perform(patch("/v1/bot/coupons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("이미 사용된 쿠폰입니다."));
     }
 
     @Test
@@ -85,6 +90,48 @@ public class CouponApiTest {
         mockMvc.perform(patch("/v1/bot/coupons")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("유저 정보가 없습니다."));
+    }
+
+    @Test
+    void 만료된_쿠폰_사용() throws Exception{
+        String snowflake = String.valueOf(ThreadLocalRandom.current().nextInt(10000000, 100000000));
+        User user = userRepository.save(User.of(snowflake,"name","url"));
+        Coupon coupon = couponRepository.save(Coupon.create(5000,-1));
+
+        CouponRedeemRequestDto dto = new CouponRedeemRequestDto(snowflake,coupon.getId().toString());
+        mockMvc.perform(patch("/v1/bot/coupons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("이미 만료된 쿠폰입니다"));
+    }
+    @Test
+    void 널_바디_체크() throws Exception {
+        String snowflake = String.valueOf(ThreadLocalRandom.current().nextInt(10000000, 100000000));
+        User user = userRepository.save(User.of(snowflake, "name", "url"));
+        Coupon coupon = couponRepository.save(Coupon.create(5000, -1));
+
+        //usersnowfalke null
+        CouponRedeemRequestDto userSnowflakeDtoNull = new CouponRedeemRequestDto(null, coupon.getId().toString());
+        mockMvc.perform(patch("/v1/bot/coupons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userSnowflakeDtoNull)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("유저 정보가 없습니다."));
+        //codId null
+        CouponRedeemRequestDto codeDtoNull = new CouponRedeemRequestDto(snowflake, null);
+        mockMvc.perform(patch("/v1/bot/coupons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(codeDtoNull)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("유효한 코드가 아닙니다."));
+        //allNull
+        CouponRedeemRequestDto allDtoNull = new CouponRedeemRequestDto(null, null);
+        mockMvc.perform(patch("/v1/bot/coupons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(allDtoNull)))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("유저 정보가 없습니다."));
     }
