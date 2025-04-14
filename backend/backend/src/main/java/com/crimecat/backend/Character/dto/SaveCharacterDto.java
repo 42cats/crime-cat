7c1dc6eabf2069a7cc2ab65234f2a64b2079f65a
sabyun <hoone0802@gmail.com>
2025-04-14 20:34:43 +0900

    be/oauth db, entity

---
+++ backend/backend/src/main/java/com/crimecat/backend/Character/dto/SaveCharacterDto.java
@@ -0,0 +1,17 @@
+package com.crimecat.backend.character.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SaveCharacterDto {
+	private UUID characterId;
+	private String guildSnowflake;
+	private String characterName;
+	private List<String> roles;
+	private LocalDateTime createdAt;
+}
