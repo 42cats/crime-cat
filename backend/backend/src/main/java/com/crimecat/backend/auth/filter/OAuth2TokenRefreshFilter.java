29628ad9ed4be7bdd488d3ecdd527a081b1d2f84
sabyun <hoone0802@gmail.com>
2025-04-18 14:13:02 +0900

    hot fix

--- backend/backend/src/main/java/com/crimecat/backend/auth/filter/OAuth2TokenRefreshFilter.java
+++ backend/backend/src/main/java/com/crimecat/backend/auth/filter/OAuth2TokenRefreshFilter.java
@@ -1,17 +1,10 @@
 package com.crimecat.backend.auth.filter;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.crimecat.backend.auth.dto.DiscordTokenResponse;
-import com.crimecat.backend.auth.oauthUser.DiscordOAuth2User;
-import com.crimecat.backend.auth.service.DiscordTokenService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -20,12 +13,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @RequiredArgsConstructor
 public class OAuth2TokenRefreshFilter extends OncePerRequestFilter {
-
-    private final DiscordTokenService discordTokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -33,37 +23,5 @@ public class OAuth2TokenRefreshFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
         throws ServletException, IOException{
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication != null && authentication.isAuthenticated()
-            && authentication.getPrincipal() instanceof DiscordOAuth2User){
-            DiscordOAuth2User user = (DiscordOAuth2User) authentication.getPrincipal();
-            Instant expiresAt = user.getExpiresAt();
-
-            if(expiresAt != null && isTokenExpiringSoon(expiresAt)){
-                String userId = user.getName() != null ? user.getName() : String.valueOf(user.getAttributes().get("id"));
-                log.info("🔁 Access Token 갱신 시작 (사용자 ID: {})", userId);
-                try{
-                DiscordTokenResponse discordTokenResponse = discordTokenService.refreshAccessToken(user.getRefreshToken());
-                String accessToken = discordTokenResponse.getAccessToken();
-                String refreshToken = discordTokenResponse.getRefreshToken();
-                Instant expireDate = Instant.now().plusSeconds(discordTokenResponse.getExpiresIn());
-                DiscordOAuth2User newUser = new DiscordOAuth2User(user,
-                        accessToken,
-                        refreshToken,
-                        expireDate);
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(newUser, null, newUser.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication((usernamePasswordAuthenticationToken));
-                log.info("디스코드 토큰 갱신 성공");
-                } catch (Exception e) {
-                    log.warn("디스코드 토큰 생성 실패");
-                    SecurityContextHolder.clearContext();
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "디스코드 토큰 갱신 실패");
-                    return;
-                }
-            }
-        }
-        filterChain.doFilter(request,response);
-    }
-    private boolean isTokenExpiringSoon(Instant expiresAt) {
-        return Duration.between(Instant.now(), expiresAt).toMinutes() <= 5;
     }
 }
