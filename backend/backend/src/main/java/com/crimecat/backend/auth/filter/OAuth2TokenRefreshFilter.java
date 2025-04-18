93f92d7deb95cb9a09bdef5902c8f3e2a519c7d9
sabyun <hoone0802@gmail.com>
2025-04-18 14:13:02 +0900

    hot fix

--- backend/backend/src/main/java/com/crimecat/backend/auth/filter/OAuth2TokenRefreshFilter.java
+++ backend/backend/src/main/java/com/crimecat/backend/auth/filter/OAuth2TokenRefreshFilter.java
@@ -1,22 +1,24 @@
 package com.crimecat.backend.auth.filter;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.crimecat.backend.auth.dto.DiscordTokenResponse;
 import com.crimecat.backend.auth.oauthUser.DiscordOAuth2User;
+import com.crimecat.backend.auth.service.DiscordTokenService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-import com.crimecat.backend.auth.service.DiscordTokenService;
-import com.crimecat.backend.auth.dto.DiscordTokenResponse;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
-import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
 
 
 @Slf4j
