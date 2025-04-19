002454d870eb561555ac367ca46e4d066bca7b5b
chyo1 <hyowon3856@gmail.com>
2025-04-19 10:18:14 +0900

    [BE] FEAT: https 설정 추가

--- docker/nginx/tools/nginx.sh
+++ docker/nginx/tools/nginx.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+mkdir -p /etc/nginx/ssl
+
 if [ ! -f "/etc/nginx/ssl/server.crt" ] || [ ! -f "/etc/nginx/ssl/server.key" ]; then
-	openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=sabyun/OU=sabyun/CN=KR" -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
+	openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=sabyun/OU=sabyun/CN=KR" -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt ||
+	{
+	  echo "OpenSSL 명령어가 실패했습니다."
+           exit 1
+  }
+
+  chmod 400 /etc/nginx/ssl/server.key
+  chmod 444 /etc/nginx/ssl/server.crt
 fi
 
-exec nginx -g "daemon off;"
+nginx -g "daemon off;"
