9c098a81a379d75a389160c84ed8c2b0f6a50e25
sabyun <hoone0802@gmail.com>
2025-04-19 15:06:39 +0900

    음악플레이어 페이드인 페이드 아웃 구현

--- bot/Commands/api/guild/music.js
+++ bot/Commands/api/guild/music.js
@@ -9,16 +9,16 @@ const baseUrl = process.env.BASE_URL
  * @param {Guild} guild
  * @param {String} channelId  
  */
-async function addGuildMusic(guildId, {title,url,thumbnail,duration}) {
+async function addGuildMusic(guildId, { title, url, thumbnail, duration }) {
 	const API_URL = `${baseUrl}/v1/bot/guilds/${guildId}/music`;  // 요청할 API 엔드포인트
 	const body = {
-			title,
-			url,
-			thumbnail,
-			duration
+		title,
+		url,
+		thumbnail,
+		duration
 	};
 	try {
-		const response = await axios.post(API_URL,body,{
+		const response = await axios.post(API_URL, body, {
 			headers: {
 				'Authorization': `Bearer ${BEARER_TOKEN}`,
 				'Content-Type': 'application/json'  // JSON 형식 요청
@@ -27,7 +27,7 @@ async function addGuildMusic(guildId, {title,url,thumbnail,duration}) {
 		console.log('응답 데이터:', response.status, response.data);
 		return response.data;
 	} catch (error) {
-		console.error('API 요청 실패:',  error.response?.data, error.response?.data?.message);
+		console.error('API 요청 실패:', error.response?.data, error.response?.data?.message);
 	}
 }
 
@@ -38,7 +38,7 @@ async function addGuildMusic(guildId, {title,url,thumbnail,duration}) {
 async function deleteGuildMusic(guildId, title) {
 	const API_URL = `${baseUrl}/v1/bot/guilds/${guildId}/music`;  // 요청할 API 엔드포인트
 	try {
-		const response = await axios.delete(API_URL,{
+		const response = await axios.delete(API_URL, {
 			headers: {
 				'Authorization': `Bearer ${BEARER_TOKEN}`,
 				'title': encodeURI(title)
@@ -55,18 +55,18 @@ async function deleteGuildMusic(guildId, title) {
  * @param {Guild} guild
  * @param {String} channelId  
  */
-async function 	getGuildMusic(guildId) {
+async function getGuildMusic(guildId) {
 	const API_URL = `${baseUrl}/v1/bot/guilds/${guildId}/music`;  // 요청할 API 엔드포인트
 	try {
-		const response = await axios.get(API_URL,{
+		const response = await axios.get(API_URL, {
 			headers: {
 				'Authorization': `Bearer ${BEARER_TOKEN}`
 			}
 		});
 		console.log('응답 데이터:', response.status, response.data);
-		return response.data.musicList ? response.data.musicList:[];
+		return response.data.musicList ? response.data.musicList : [];
 	} catch (error) {
-		console.error('API 요청 실패:', error.response ? error.response.data : error.response.data.message);
+		console.error('API 요청 실패:', error.response ? error?.response?.data : error.response?.data?.message);
 		return [];
 	}
 }
@@ -74,5 +74,5 @@ async function 	getGuildMusic(guildId) {
 module.exports = {
 	addGuildMusic,
 	deleteGuildMusic,
-	getGuildMusic	
+	getGuildMusic
 }
