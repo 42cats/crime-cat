d05c348bf964581550b15c27549204b217062a6d
sabyun <hoone0802@gmail.com>
2025-04-21 19:55:45 +0900

    main rebased and run test on dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,49 +3,49 @@ import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 
 export default defineConfig({
-    // server: {
-    //     host: "::",
-    //     port: 5173,
-    //     proxy: {
-    //         "/api": {
-    //             target: "http://localhost:8080",
-    //             changeOrigin: true,
-    //             configure(proxy) {
-    //                 proxy.on("proxyReq", (proxyReq) => {
-    //                     proxyReq.setHeader(
-    //                         "X-Forwarded-Host",
-    //                         "localhost:5173"
-    //                     );
-    //                 });
-    //             },
-    //         },
-    //         "/oauth2": {
-    //             target: "http://localhost:8080",
-    //             changeOrigin: true,
-    //             // 여기만 추가
-    //             configure(proxy) {
-    //                 proxy.on("proxyReq", (proxyReq) => {
-    //                     proxyReq.setHeader(
-    //                         "X-Forwarded-Host",
-    //                         "localhost:5173"
-    //                     );
-    //                 });
-    //             },
-    //         },
-    //         "/login/oauth2": {
-    //             target: "http://localhost:8080",
-    //             changeOrigin: true,
-    //             configure(proxy) {
-    //                 proxy.on("proxyReq", (proxyReq) => {
-    //                     proxyReq.setHeader(
-    //                         "X-Forwarded-Host",
-    //                         "localhost:5173"
-    //                     );
-    //                 });
-    //             },
-    //         },
-    //     },
-    // },
+    server: {
+        host: "::",
+        port: 5173,
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                configure(proxy) {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        proxyReq.setHeader(
+                            "X-Forwarded-Host",
+                            "localhost:5173"
+                        );
+                    });
+                },
+            },
+            "/oauth2": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                // 여기만 추가
+                configure(proxy) {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        proxyReq.setHeader(
+                            "X-Forwarded-Host",
+                            "localhost:5173"
+                        );
+                    });
+                },
+            },
+            "/login/oauth2": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                configure(proxy) {
+                    proxy.on("proxyReq", (proxyReq) => {
+                        proxyReq.setHeader(
+                            "X-Forwarded-Host",
+                            "localhost:5173"
+                        );
+                    });
+                },
+            },
+        },
+    },
     plugins: [react()],
     resolve: {
         alias: {
