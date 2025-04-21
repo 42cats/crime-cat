d7849c98b53f6223c0f5713b06d8a07d3bd150a6
sabyun <sabyun@sabyun42>
2025-04-21 17:05:11 +0900

    health check

--- backend/backend/src/main/java/com/crimecat/backend/config/SecurityConfig.java
+++ backend/backend/src/main/java/com/crimecat/backend/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) /// 세션인증 끔
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/oauth2/**","/api/v1/**").permitAll()
+                        .requestMatchers("/actuator/health", "/actuator/info", "/oauth2/**","/api/v1/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
