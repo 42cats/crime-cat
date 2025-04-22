09b3a25de016fc7a395c1bc666e2933ae3f3183c
smj53 <smjeong53@gmail.com>
2025-04-22 15:01:50 +0900

    add User entity

--- backend/backend/src/main/java/com/crimecat/backend/bot/user/domain/User.java
+++ backend/backend/src/main/java/com/crimecat/backend/bot/user/domain/User.java
@@ -1,16 +1,19 @@
 package com.crimecat.backend.bot.user.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import com.crimecat.backend.web.webUser.domain.WebUser;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "USERS")
@@ -18,7 +21,26 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public class User {
+    @Id
+    @UuidGenerator
+    @GeneratedValue
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(name = "ID", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @JoinColumn(name = "WEB_USER_ID", referencedColumnName = "ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private WebUser webUser;
+
+    @JoinColumn(name = "DISCORD_USER_ID", referencedColumnName = "ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private DiscordUser discordUser;
+
     @CreatedDate
     @Column(name = "CREATED_AT", updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
 }
