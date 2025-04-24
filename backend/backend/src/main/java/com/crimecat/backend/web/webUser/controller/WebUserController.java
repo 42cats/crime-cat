ab509d9b59ed1e4b50f3aaf6492435e8d5ddfdff
sabyun <hoone0802@gmail.com>
2025-04-24 10:56:59 +0900

    webcoupon use userid

--- backend/backend/src/main/java/com/crimecat/backend/web/webUser/controller/WebUserController.java
+++ backend/backend/src/main/java/com/crimecat/backend/web/webUser/controller/WebUserController.java
@@ -2,9 +2,14 @@ package com.crimecat.backend.web.webUser.controller;
 
 import com.crimecat.backend.bot.coupon.dto.CouponRedeemRequestDto;
 import com.crimecat.backend.bot.coupon.dto.MessageDto;
+import com.crimecat.backend.bot.coupon.dto.WebCouponRequestDto;
 import com.crimecat.backend.bot.coupon.service.CouponService;
+import com.crimecat.backend.bot.user.domain.User;
+import com.crimecat.backend.bot.user.repository.UserRepository;
+import com.crimecat.backend.exception.ErrorStatus;
 import com.crimecat.backend.web.webUser.service.WebUserService;
 import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +21,7 @@ public class WebUserController {
 
   private final WebUserService webUserService;
   private final CouponService couponService;
+  private final UserRepository userRepository;
 
   @PostMapping("/daily_check/{user_id}")
   public ResponseEntity<Map<String, Object>> dailyCheck(@PathVariable("user_id") String userId) {
@@ -26,7 +32,11 @@ public class WebUserController {
     return webUserService.isDailyCheck(userId);
   }
   @PatchMapping("/coupons")
-  public MessageDto<?> couponReem (@RequestBody CouponRedeemRequestDto request){
-    return couponService.redeemCoupon(request);
+  public MessageDto<?> couponReem (@RequestBody WebCouponRequestDto request){
+    User user = userRepository.findById(UUID.fromString(request.getUserId()))
+        .orElseThrow(ErrorStatus.USER_NOT_FOUND::asControllerException);
+    CouponRedeemRequestDto couponRedeemRequestDto = new CouponRedeemRequestDto(
+        user.getDiscordSnowflake(), request.getCode());
+    return couponService.redeemCoupon(couponRedeemRequestDto);
   }
 }
