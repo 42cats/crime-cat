5579dc5736cf81d01dffe36dbf507520f3741c7f
sabyun <hoone0802@gmail.com>
2025-04-26 11:55:07 +0900

    정렬 메소드및 유틸 생성

---
+++ backend/backend/src/main/java/com/crimecat/backend/web/gameHistory/sort/GameHistorySortType.java
@@ -0,0 +1,22 @@
+package com.crimecat.backend.web.gameHistory.sort;
+
+import com.crimecat.backend.auth.util.sort.SortType;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+public enum GameHistorySortType implements SortType {
+  LATEST(Sort.by(Sort.Direction.DESC, "createdAt")),
+  OLDEST(Sort.by(Direction.ASC, "createdAt")),
+  ;
+
+  private final Sort sort;
+
+  GameHistorySortType(Sort sort) {
+    this.sort = sort;
+  }
+
+  @Override
+  public Sort getSort() {
+    return sort;
+  }
+}
