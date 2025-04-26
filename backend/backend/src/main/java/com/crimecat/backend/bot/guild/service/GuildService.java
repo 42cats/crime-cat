7ce8772c8f217a148119ff2cbf8d894488cea657
sabyun <hoone0802@gmail.com>
2025-04-27 06:53:29 +0900

    트렌젝션 걸기 길드추가시

--- backend/backend/src/main/java/com/crimecat/backend/bot/guild/service/GuildService.java
+++ backend/backend/src/main/java/com/crimecat/backend/bot/guild/service/GuildService.java
@@ -13,6 +13,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -28,6 +29,7 @@ public class GuildService {
      * @param guildDto 생성할 길드 정보 (snowflake, name, owner snowflake)
      * @return 길드 생성 정보 반환 MessageDto
      */
+    @Transactional
     public MessageDto<GuildResponseDto> addGuild(GuildDto guildDto) {
         User user = userRepository.findByDiscordSnowflake(guildDto.getOwnerSnowflake()).orElseThrow(
             ErrorStatus.USER_NOT_FOUND::asServiceException);
@@ -50,6 +52,7 @@ public class GuildService {
      * 길드 삭제
      * @param snowflake 삭제할 길드 snowflake
      */
+    @Transactional
     public void deleteGuild(String snowflake) {
         Guild guild = guildRepository.findBySnowflake(snowflake).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Guild not found")
@@ -64,14 +67,17 @@ public class GuildService {
     /*
 		gameHistory 저장 확인용 임시 메서드
 	 */
+    @Transactional
     public Guild findGuildByGuildSnowflake(String guildSnowflake) {
         return guildRepository.findGuildByGuildSnowflake(guildSnowflake).orElse(null);
     }
 
+    @Transactional
     public List<Guild> findGuildByOwnerId(String ownerSnowFlake){
         return guildRepository.findActiveGuildsByOwner(ownerSnowFlake);
     }
 
+    @Transactional
     public List<Guild> findAllGuild(){
         return guildRepository.findAllActiveGuilds();
     }
