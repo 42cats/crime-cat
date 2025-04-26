438b43e27d04aa320f492c6bb28a7c788d487101
sabyun <hoone0802@gmail.com>
2025-04-27 04:28:56 +0900

    notice error fix

--- backend/backend/src/main/java/com/crimecat/backend/web/admin/service/AdminService.java
+++ backend/backend/src/main/java/com/crimecat/backend/web/admin/service/AdminService.java
@@ -1,7 +1,7 @@
 package com.crimecat.backend.web.admin.service;
 
 import com.crimecat.backend.exception.ErrorStatus;
-import com.crimecat.backend.web.notice.domain.Notice;
+import com.crimecat.backend.web.notice.domain.Notices;
 import com.crimecat.backend.web.notice.dto.NoticeReorderRequestDto;
 import com.crimecat.backend.web.notice.dto.NoticeRequestDto;
 import com.crimecat.backend.web.notice.repository.NoticeRepository;
@@ -20,14 +20,14 @@ public class AdminService {
   @Transactional
   public void addNotice(NoticeRequestDto noticeRequestDto) {
     try{
-      Notice notice = Notice.builder()
+      Notices notices = Notices.builder()
           .title(noticeRequestDto.getTitle())
           .content(noticeRequestDto.getContent())
           .noticeType(noticeRequestDto.getNoticeType())
           .summary(noticeRequestDto.getSummary())
           .isPinned(noticeRequestDto.getIsPinned())
           .build();
-      noticeRepository.save(notice);
+      noticeRepository.save(notices);
       } catch (Exception e) {
         throw ErrorStatus.INVALID_INPUT.asServiceException();
     }
@@ -35,16 +35,16 @@ public class AdminService {
 
   @Transactional
   public void patchNotice(UUID uuid, NoticeRequestDto noticeRequestDto){
-      Notice notice = noticeRepository.findById(uuid)
+      Notices notices = noticeRepository.findById(uuid)
           .orElseThrow(ErrorStatus.RESOURCE_NOT_FOUND::asServiceException);
     try{
-      notice.setNoticeType(noticeRequestDto.getNoticeType());
-      notice.setSummary(noticeRequestDto.getSummary());
-      notice.setContent(noticeRequestDto.getContent());
-      notice.setTitle(noticeRequestDto.getTitle());
-      notice.setIsPinned(noticeRequestDto.getIsPinned());
+      notices.setNoticeType(noticeRequestDto.getNoticeType());
+      notices.setSummary(noticeRequestDto.getSummary());
+      notices.setContent(noticeRequestDto.getContent());
+      notices.setTitle(noticeRequestDto.getTitle());
+      notices.setIsPinned(noticeRequestDto.getIsPinned());
 
-      noticeRepository.save(notice);
+      noticeRepository.save(notices);
 
     }
     catch (Exception e) {
@@ -62,14 +62,14 @@ public class AdminService {
 
     for (NoticeReorderRequestDto noticeReorderRequestDto : noticeReorderRequestDtoList) {
       UUID uuid = UUID.fromString(noticeReorderRequestDto.getUuid());
-      Notice notice = noticeRepository.findById(uuid)
+      Notices notices = noticeRepository.findById(uuid)
           .orElseThrow(ErrorStatus.RESOURCE_NOT_FOUND::asServiceException);
       if(noticeReorderRequestDto.getIsPinned()){
-        notice.setOrderIdx(noticeReorderRequestDto.getOrderIdx());
+        notices.setOrderIdx(noticeReorderRequestDto.getOrderIdx());
       }
       else{
-        notice.setIsPinned(false);
-        notice.setOrderIdx(0);
+        notices.setIsPinned(false);
+        notices.setOrderIdx(0);
       }
     }
   }
