23c9705ff012341c07b9955b75d8346b66c540d7
sabyun <sabyun@sabyun42>
2025-04-27 02:55:50 +0900

    aa

--- backend/backend/src/main/java/com/crimecat/backend/config/WebConfig.java
+++ backend/backend/src/main/java/com/crimecat/backend/config/WebConfig.java
@@ -14,8 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든경로에 CORS 적용
-                .allowedOrigins( "https://" + serviceUrlConfig.getDomain()) // 접근허용 주소
+                .allowedOrigins( "https://" + serviceUrlConfig.getDomain(),"https://www." + serviceUrlConfig.getDomain()) // 접근허용 주소
                 .allowCredentials(true)  //쿠키 및 인증 헤더 적용
-                .allowedMethods("*"); //post, get, put ,delete 다 적용
+                .allowedMethods("*") //post, get, put ,delete 다 적용
+                .allowedHeaders("*");
     }
 }
