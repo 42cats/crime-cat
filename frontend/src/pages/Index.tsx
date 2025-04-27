145d53a59a110f8efdfb4c83f139ad7dabfdf56d
sabyun <sabyun@sabyun42>
2025-04-27 19:45:30 +0900

    main beta page

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -5,12 +5,14 @@ import AnnouncementSection from "@/components/home/AnnouncementSection";
 import LatestPostsSection from "@/components/home/LatestPostsSection";
 import GameAdsSection from "@/components/home/GameAdsSection";
 import CurrentGamesSection from "@/components/home/CurrentGamesSection";
+import BotAddSection from "@/components/home/BotAddSection";
 
 const Index: React.FC = () => {
     return (
         <PageTransition>
             <div className="pt-20">
-                <BotStatsSection />
+                <BotAddSection />
+                {/* <BotStatsSection /> */}
                 <AnnouncementSection />
                 {/* <LatestPostsSection />
         <GameAdsSection />
