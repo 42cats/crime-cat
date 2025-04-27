26dd1015e5f1a9bd6c70fe5082f9b8d94a0e1f71
변상훈 <sabyun@byeonsanghun-ui-Macmini.local>
2025-04-27 15:14:38 +0900

    persnal info error edit

--- backend/backend/src/main/java/com/crimecat/backend/web/stats/service/WebStatsInfoService.java
+++ backend/backend/src/main/java/com/crimecat/backend/web/stats/service/WebStatsInfoService.java
@@ -12,6 +12,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Supplier;
+
+import com.crimecat.backend.web.webUser.domain.WebUser;
+import com.crimecat.backend.web.webUser.repository.WebUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -26,6 +29,7 @@ public class WebStatsInfoService {
   private final GameHistoryRepository gameHistoryRepository;
   private final WebStatsInfoServiceProxy webStatsInfoServiceProxy;
   private final RedisInfoDb redisInfoDb;
+  private final WebUserRepository webUserRepository;
 
   @Transactional(readOnly = true)
   public ResponseEntity<Map<String, String>> mainStatInfo() {
@@ -68,13 +72,10 @@ public class WebStatsInfoService {
   }
 
   public ResponseEntity<Map<String, String>> personalInfoOnDashBord(String id) {
-    User user =
-        userRepository
-            .findById(UUID.fromString(id))
-            .orElseThrow(ErrorStatus.USER_NOT_FOUND::asServiceException);
+    WebUser webUser = webUserRepository.findById(UUID.fromString(id)).orElseThrow(ErrorStatus.USER_NOT_FOUND::asServiceException);
 
     Map<String, String> userPersonalGameInfo =
-        webStatsInfoServiceProxy.getUserPersonalGameInfo(user);
+        webStatsInfoServiceProxy.getUserPersonalGameInfo(webUser.getUser());
     return ResponseEntity.ok().body(userPersonalGameInfo);
   }
   }
