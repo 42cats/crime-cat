c87df99d560a1ef19c57efad5a5df0229a8d26d7
변상훈 <sabyun@byeonsanghun-ui-Macmini.local>
2025-04-27 23:17:49 +0900

    notice entity edit

--- backend/backend/src/main/java/com/crimecat/backend/web/notice/repository/NoticeRepository.java
+++ backend/backend/src/main/java/com/crimecat/backend/web/notice/repository/NoticeRepository.java
@@ -1,7 +1,7 @@
 package com.crimecat.backend.web.notice.repository;
 
 
-import com.crimecat.backend.web.notice.domain.Notices;
+import com.crimecat.backend.web.notice.domain.Notice;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
@@ -9,13 +9,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface NoticeRepository extends JpaRepository<Notices, UUID> {
+public interface NoticeRepository extends JpaRepository<Notice, UUID> {
 
-  @Query("SELECT n FROM Notices n WHERE n.isPinned = true ORDER BY n.orderIdx ASC , n.createdAt DESC")
-  List<Notices> findPinnedNoticesOrdered(Pageable pageable); // 고정공지만 Orderidx 순 최신순
+  @Query("SELECT n FROM Notice n WHERE n.isPinned = true ORDER BY n.orderIdx ASC , n.createdAt DESC")
+  List<Notice> findPinnedNoticesOrdered(Pageable pageable); // 고정공지만 Orderidx 순 최신순
 
-  @Query("SELECT n FROM Notices n ORDER BY n.isPinned DESC, n.orderIdx ASC, n.createdAt DESC")
-  Page<Notices> findAllNoticesOrdered(Pageable pageable);
+  @Query("SELECT n FROM Notice n ORDER BY n.isPinned DESC, n.orderIdx ASC, n.createdAt DESC")
+  Page<Notice> findAllNoticesOrdered(Pageable pageable);
 
 
 }
