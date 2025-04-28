ef5c018cb1003c5d5631c8d12b173a8653e5e997
sabyun <hoone0802@gmail.com>
2025-04-28 14:13:38 +0900

    entity edit

--- backend/backend/src/main/java/com/crimecat/backend/web/gametheme/dto/AddGameThemeRequest.java
+++ backend/backend/src/main/java/com/crimecat/backend/web/gametheme/dto/AddGameThemeRequest.java
@@ -28,7 +28,8 @@ public class AddGameThemeRequest {
     private String content; // 게시글 본문
     private int playerMin; // 최소 인원수
     private int playerMax;
-    private int playtime; // 소요시간 (분)
+    private int playtimeMin; // 소요시간 (분)
+    private int playtimeMax; // 소요시간 (분)
     private int price; // 금액 (원화)
     private int difficulty; // 난이도 1~10
     private boolean publicStatus; // 공개 여부
