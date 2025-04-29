088237e0706b7b533c7c0f0c64f9d14b6832eebc
sabyun <hoone0802@gmail.com>
2025-04-29 18:32:18 +0900

    프로필 api 작성중

---
+++ backend/backend/src/main/java/com/crimecat/backend/web/webUser/dto/WebUserProfileEditRequestDto.java
@@ -0,0 +1,12 @@
+package com.crimecat.backend.web.webUser.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class WebUserProfileEditRequestDto {
+  private 
+}
