f945c6487b0a63130d48a6a5d12506419bae7fd7
smj <smjeong53@gmail.com>
2025-04-29 16:31:36 +0900

    [BE] FEAT: 게임 테마 좋아요(추천) api #134

---
+++ backend/backend/src/main/java/com/crimecat/backend/web/gametheme/dto/GetLikeStatusResponse.java
@@ -0,0 +1,10 @@
+package com.crimecat.backend.web.gametheme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GetLikeStatusResponse {
+    private boolean status;
+}
