4d8c521e72a54edddaa2460a1c257560630a7567
sabyun <hoone0802@gmail.com>
2025-05-02 16:50:52 +0900

    feat: CSRF 보호를 위한 CsrfCookieFilter 추가 및 CORS 설정 수정

--- backend/backend/src/main/java/com/crimecat/backend/config/WebConfig.java
+++ backend/backend/src/main/java/com/crimecat/backend/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든경로에 CORS 적용
                 .allowedOrigins( "https://" + serviceUrlConfig.getDomain(),"https://www." + serviceUrlConfig.getDomain(), "http://localhost:5173") // 접근허용 주소
                 .allowCredentials(true)  //쿠키 및 인증 헤더 적용
-                .allowedMethods("*") //post, get, put ,delete 다 적용
-                .allowedHeaders("*")
-                .maxAge(3600);
+                .exposedHeaders("X-CSRF-TOKEN") // 클라이언트가 응답 헤더에서 CSRF 토큰 읽을 수 있게 허용
+                .allowCredentials(true) // 쿠키 및 인증 헤더 포함 허용
+                .maxAge(3600); // preflight 캐시 시간 (초)
     }
 }
