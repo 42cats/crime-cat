530ee6e2f1632a8332bf538fb11ef2b3f97c6f0e
sabyun <hoone0802@gmail.com>
2025-05-04 15:41:45 +0900

    feat: CsrfTokenConfig 및 SecurityConfig 수정하여 CSRF 보호 기능 개선

--- backend/backend/src/main/java/com/crimecat/backend/config/CsrfTokenConfig.java
+++ backend/backend/src/main/java/com/crimecat/backend/config/CsrfTokenConfig.java
@@ -1,13 +1,18 @@
 package com.crimecat.backend.config;
 
 
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 @Configuration
+@RequiredArgsConstructor
 public class CsrfTokenConfig {
+
+  private final ServiceUrlConfig serviceUrlConfig;
 
   @Bean
   public CsrfTokenRepository csrfTokenRepository(){
@@ -15,6 +20,15 @@ public class CsrfTokenConfig {
     repository.setCookieName("XSRF-TOKEN");
     repository.setHeaderName("X-XSRF-TOKEN");
     repository.setCookiePath("/");
+
+    repository.setCookieCustomizer(
+        responseCookieBuilder ->
+            responseCookieBuilder
+                .maxAge(Duration.ofHours(1))
+                .sameSite("Strict")
+                .domain(serviceUrlConfig.getDomain())
+                .secure(true)
+    );
     return repository;
   }
 }
