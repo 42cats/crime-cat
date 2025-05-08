721263ce7185d055c78be84129235347d47d2259
sabyun <hoone0802@gmail.com>
2025-05-08 14:25:10 +0900

    feat: 웹유저에 이메일 알림 설정 컬럼 추가 feat: maker_teams 테이블에 개인 팀 여부 컬럼 추가 feat: maker_team_members 테이블의 user_id를 web_user_id로 변경하고 팀장 여부 컬럼 추가 feat: game_themes 테이블의 type 컬럼을 VARCHAR로 변경하고 author FK를 web_users로 업데이트 feat: game_theme_recommendations 테이블의 user_id를 web_user_id로 변경 및 FK·UNIQUE 키 갱신

---
+++ docker/mariadb/db/migrations/V1.1.0/V1.1.0_001_add_emai_alret_on_web_user_table.sql
@@ -0,0 +1,11 @@
+-- Migration: V1.1.0_001_add_emai_alret_on_web_user_table.sql
+-- Description: 웹유저에 이메일 알림 설정여부 추가
+-- Created: 2025-05-08 14:30:00
+
+USE ${DB_DISCORD};
+
+ALTER TABLE `web_users`
+  ADD COLUMN `email_alarm` BIT(1) NOT NULL DEFAULT b'0'
+    COMMENT '이메일 수신설정'
+    AFTER `social_links`;
+가
