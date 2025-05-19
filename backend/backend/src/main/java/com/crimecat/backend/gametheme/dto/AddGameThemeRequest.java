7f1bce3270ee5d0633eb310712d21ff30c48a5e4
smj53 <smjeong53@gmail.com>
2025-05-20 02:47:51 +0900

    [BE] FEAT: 게임 테마 추가 / 수정 validation 추가

--- backend/backend/src/main/java/com/crimecat/backend/gametheme/dto/AddGameThemeRequest.java
+++ backend/backend/src/main/java/com/crimecat/backend/gametheme/dto/AddGameThemeRequest.java
@@ -1,10 +1,18 @@
 package com.crimecat.backend.gametheme.dto;
 
 import com.crimecat.backend.gametheme.enums.ThemeType;
+import com.crimecat.backend.gametheme.validator.MinMaxCheck;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
@@ -15,19 +23,28 @@ import lombok.Data;
         @JsonSubTypes.Type(value = AddCrimesceneThemeRequest.class, name = ThemeType.Values.CRIMESCENE),
 })
 @Data
+@MinMaxCheck(min = "playerMin", max = "playerMax", message = "인원수 최소값이 최대값보다 큽니다.")
+@MinMaxCheck(min = "playtimeMin", max = "playtimeMax", message = "플레이 시간 최소값이 최대값보다 큽니다.")
 public class AddGameThemeRequest {
+    @NotBlank
     private String title; //"테마 제목"
+    @NotBlank
     private String summary; // 간략 설명
-    private Set<String> tags; // 태그 배열
+    @Size(min = 1)
+    private Set<String> tags = new HashSet<>(); // 태그 배열
+    @NotBlank
     private String content; // 게시글 본문
+    @Min(1)
     private int playerMin; // 최소 인원수
     private int playerMax;
+    @Min(1)
     private int playtimeMin; // 소요시간 (분)
     private int playtimeMax; // 소요시간 (분)
+    @Min(0)
     private int price; // 금액 (원화)
-    private int difficulty; // 난이도 1~10
+    @Range(min = 1, max = 5)
+    private int difficulty; // 난이도 1~5
     private boolean publicStatus; // 공개 여부
-    private String type;
     private boolean recommendationEnabled;
     private boolean commentEnabled;
 }
--- backend/backend/src/main/java/com/crimecat/backend/gametheme/dto/UpdateGameThemeRequest.java
+++ backend/backend/src/main/java/com/crimecat/backend/gametheme/dto/UpdateGameThemeRequest.java
@@ -2,14 +2,19 @@ package com.crimecat.backend.gametheme.dto;
 
 import com.crimecat.backend.gametheme.domain.GameTheme;
 import com.crimecat.backend.gametheme.enums.ThemeType;
+import com.crimecat.backend.gametheme.validator.MinMaxCheck;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import java.util.Set;
 import java.util.function.Consumer;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.openapitools.jackson.nullable.JsonNullable;
 
 @JsonTypeInfo(
@@ -23,20 +28,29 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@MinMaxCheck(min = "playerMin", max = "playerMax", message = "인원수 최소값이 최대값보다 큽니다.")
+@MinMaxCheck(min = "playtimeMin", max = "playtimeMax", message = "플레이 시간 최소값이 최대값보다 큽니다.")
 public class UpdateGameThemeRequest {
+    @Size(min = 1)
     private String title;
+    @Size(min = 1)
     private String summary;
     private JsonNullable<String> thumbnail = JsonNullable.undefined();
+    @Size(min = 1)
     private Set<String> tags;
+    @Size(min = 1)
     private String content;
+    @Min(1)
     private Integer playerMin;
     private Integer playerMax;
-    private Integer playTimeMin;
-    private Integer playTimeMax;
+    @Min(1)
+    private Integer playtimeMin;
+    private Integer playtimeMax;
+    @Min(0)
     private Integer price;
+    @Range(min = 1, max = 5)
     private Integer difficulty;
     private Boolean publicStatus;
-    private String type;
     private Boolean recommendationEnabled;
     private Boolean commentEnabled;
 
@@ -59,8 +73,8 @@ public class UpdateGameThemeRequest {
         set(content, gameTheme::setContent);
         set(playerMin, gameTheme::setPlayerMin);
         set(playerMax, gameTheme::setPlayerMax);
-        set(playTimeMin, gameTheme::setPlayTimeMin);
-        set(playTimeMax, gameTheme::setPlayTimeMax);
+        set(playtimeMin, gameTheme::setPlayTimeMin);
+        set(playtimeMax, gameTheme::setPlayTimeMax);
         set(price, gameTheme::setPrice);
         set(difficulty, gameTheme::setDifficulty);
         set(publicStatus, gameTheme::setPublicStatus);
