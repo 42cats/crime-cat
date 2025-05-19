b7d0bb08e9d771b55e14e8279553aed485d44d05
smj53 <smjeong53@gmail.com>
2025-05-20 08:08:42 +0900

    [BE] FIX: 게임 테마 정렬 추가, 키워드 검색

--- backend/backend/src/main/java/com/crimecat/backend/gametheme/sort/GameThemeSortType.java
+++ backend/backend/src/main/java/com/crimecat/backend/gametheme/sort/GameThemeSortType.java
@@ -5,7 +5,18 @@ import org.springframework.data.domain.Sort;
 
 public enum GameThemeSortType implements SortType {
     LATEST(Sort.by(Sort.Direction.DESC, "createdAt")),
+    OLDEST(LATEST.getSort().reverse()),
     RECOMMENDATION_ENABLED(Sort.by(Sort.Direction.DESC, "recommendationEnabled")),
+    LIKE(Sort.by("recommendations")),
+    LIKE_DESC(LIKE.getSort().reverse()),
+    VIEW(Sort.by("views")),
+    VIEW_DESC(VIEW.getSort().reverse()),
+    PRICE(Sort.by("price")),
+    PRICE_DESC(PRICE.getSort().reverse()),
+    PLAYTIME(Sort.by("playTimeMax", "playTimeMin")),
+    PLAYTIME_DESC(PLAYTIME.getSort().reverse()),
+    DEFAULT(Sort.by(Sort.Direction.DESC, "recommendationEnabled")
+            .and(Sort.by(Sort.Direction.DESC, "createdAt"))),
     ;
 
     private final Sort sort;
--- backend/backend/src/main/java/com/crimecat/backend/gametheme/specification/GameThemeSpecification.java
+++ backend/backend/src/main/java/com/crimecat/backend/gametheme/specification/GameThemeSpecification.java
@@ -1,9 +1,17 @@
 package com.crimecat.backend.gametheme.specification;
 
+import com.crimecat.backend.gametheme.domain.CrimesceneTheme;
 import com.crimecat.backend.gametheme.domain.GameTheme;
+import com.crimecat.backend.gametheme.domain.MakerTeam;
+import com.crimecat.backend.gametheme.domain.MakerTeamMember;
 import com.crimecat.backend.gametheme.enums.ThemeType;
+import com.crimecat.backend.webUser.domain.WebUser;
+import io.micrometer.common.util.StringUtils;
+import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class GameThemeSpecification {
@@ -20,5 +28,35 @@ public class GameThemeSpecification {
                         criteriaBuilder.equal(root.get("authorId"), userId)
                 )
             );
+    }
+
+    public static Specification<GameTheme> findKeyword(String keyword, String category) {
+        return (root, query, criteriaBuilder) -> {
+            if (StringUtils.isEmpty(keyword)) {
+                return criteriaBuilder.conjunction();
+            }
+            List<Predicate> predicates = new ArrayList<>();
+//            Join<GameTheme, WebUser> authorJoin = root.join("author", JoinType.LEFT);
+            String pattern = String.format("%%%s%%", keyword);
+            predicates.add(criteriaBuilder.or(
+                    criteriaBuilder.like(root.get("title"), pattern),
+                    criteriaBuilder.like(root.get("content"), pattern),
+                    criteriaBuilder.like(root.get("summary"), pattern),
+                    criteriaBuilder.like(root.get("tags"), pattern),
+                    criteriaBuilder.like(root.get("author").get("nickname"), pattern)
+//                    criteriaBuilder.like(authorJoin.get("nickname"), pattern)
+            ));
+            if (ThemeType.Values.CRIMESCENE.equals(category)) {
+                Root<CrimesceneTheme> crimesceneThemeRoot = criteriaBuilder.treat(root, CrimesceneTheme.class);
+                Join<CrimesceneTheme, MakerTeam> teamJoin = crimesceneThemeRoot.join("team", JoinType.LEFT);
+                Join<MakerTeam, MakerTeamMember> memberJoin = teamJoin.join("members", JoinType.LEFT);
+                predicates.add(criteriaBuilder.or(
+                        criteriaBuilder.like(teamJoin.get("name"), pattern),
+                        criteriaBuilder.like(memberJoin.get("webUser").get("nickname"), pattern),
+                        criteriaBuilder.like(memberJoin.get("name"), pattern)
+                ));
+            }
+            return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
+        };
     }
 }
