c078033792f4ac9f8b19714576c367cc332ef7f7
hyeongsh <shinhs6384@naver.com>
2025-05-20 13:49:54 +0900

    feat: boardPostDetail api 생성

--- backend/backend/src/main/java/com/crimecat/backend/boardPost/service/BoardPostService.java
+++ backend/backend/src/main/java/com/crimecat/backend/boardPost/service/BoardPostService.java
@@ -1,11 +1,16 @@
 package com.crimecat.backend.boardPost.service;
 
 import com.crimecat.backend.boardPost.domain.BoardPost;
+import com.crimecat.backend.boardPost.dto.BoardPostDetailResponse;
 import com.crimecat.backend.boardPost.dto.BoardPostResponse;
 import com.crimecat.backend.boardPost.enums.BoardType;
 import com.crimecat.backend.boardPost.enums.PostType;
 import com.crimecat.backend.boardPost.repository.BoardPostLikeRepository;
 import com.crimecat.backend.boardPost.repository.BoardPostRepository;
+import com.crimecat.backend.postComment.domain.PostComment;
+import com.crimecat.backend.postComment.dto.PostCommentResponse;
+import com.crimecat.backend.postComment.repository.PostCommentLikeRepository;
+import com.crimecat.backend.postComment.repository.PostCommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +18,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -34,5 +44,18 @@ public class BoardPostService {
         Page<BoardPost> posts = boardPostRepository.findAllByKeywordAndTypeAndIsDeletedFalse(kw, boardType, postType, pageable);
 
         return posts.map(BoardPostResponse::from);
+    }
+
+    @Transactional(readOnly = true)
+    public BoardPostDetailResponse getBoardPostDetail(
+            UUID postId,
+            UUID userId
+    ) {
+        BoardPost boardPost = boardPostRepository.getBoardPostById(postId);
+        boolean isLiked = boardPostLikeRepository.existsByUserIdAndPostId(userId, postId);
+        boolean isOwnPost = boardPost.getUserId().equals(userId);
+
+        return BoardPostDetailResponse.from(boardPost, isOwnPost, isLiked);
+
     }
 }
