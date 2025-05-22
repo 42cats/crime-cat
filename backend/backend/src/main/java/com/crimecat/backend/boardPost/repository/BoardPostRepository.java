f3968efd58792165604249f276b1ab633bb305f5
hyeongsh <shinhs6384@naver.com>
2025-05-22 16:57:38 +0900

    feat: 게시글 조회 시 유저당 조회수 30분마다 증가

--- backend/backend/src/main/java/com/crimecat/backend/boardPost/repository/BoardPostRepository.java
+++ backend/backend/src/main/java/com/crimecat/backend/boardPost/repository/BoardPostRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -41,4 +42,8 @@ public interface BoardPostRepository extends JpaRepository<BoardPost, UUID> {
     );
 
     BoardPost getBoardPostById(UUID id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE BoardPost p SET p.views = p.views + 1 WHERE p.id = :postId")
+    void incrementViews(@Param("postId") UUID postId);
 }
