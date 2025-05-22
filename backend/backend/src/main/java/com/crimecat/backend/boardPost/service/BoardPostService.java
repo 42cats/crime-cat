3918d349b8489a227a791e2c539812e33c8e4f18
hyeongsh <shinhs6384@naver.com>
2025-05-22 16:57:38 +0900

    feat: 게시글 조회 시 유저당 조회수 30분마다 증가

--- backend/backend/src/main/java/com/crimecat/backend/boardPost/service/BoardPostService.java
+++ backend/backend/src/main/java/com/crimecat/backend/boardPost/service/BoardPostService.java
@@ -12,9 +12,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Service
@@ -23,6 +25,7 @@ public class BoardPostService {
 
     private final BoardPostRepository boardPostRepository;
     private final BoardPostLikeRepository boardPostLikeRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional(readOnly = true)
     public Page<BoardPostResponse> getBoardPage(
@@ -39,14 +42,24 @@ public class BoardPostService {
         return posts.map(BoardPostResponse::from);
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public BoardPostDetailResponse getBoardPostDetail(
             UUID postId,
             UUID userId
     ) {
+        // redis를 통해 30분 동안 한번만 조회수가 올라가도록 설정
+        String redisKey = "views:" + postId + ":" + userId;
+
+        Boolean exists = redisTemplate.hasKey(redisKey);
+        if (!Boolean.TRUE.equals(exists)) {
+            boardPostRepository.incrementViews(postId);
+            redisTemplate.opsForValue().set(redisKey, String.valueOf(1), Duration.ofMinutes(30));
+        }
+
         BoardPost boardPost = boardPostRepository.getBoardPostById(postId);
         boolean isLiked = boardPostLikeRepository.existsByUserIdAndPostId(userId, postId);
         boolean isOwnPost = boardPost.getUserId().equals(userId);
+
 
         return BoardPostDetailResponse.from(boardPost, isOwnPost, isLiked);
 
