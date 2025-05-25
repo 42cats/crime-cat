f876def54f72f1d32d513f4f09f05a4868e289ca
변상훈 <hoone0802@gamil.com>
2025-05-25 22:08:01 +0900

    feat: board_posts 테이블에 CREATOR 유형 추가

---
+++ docker/mariadb/db/migrations/V1.2.10/V1.2.10_001_add_creator_board_type.sql
@@ -0,0 +1,10 @@
+-- V1.2.10_001_add_creator_board_type.sql
+-- board_posts 테이블의 board_type enum에 CREATOR 추가
+
+START TRANSACTION;
+
+-- board_type enum에 CREATOR 추가
+ALTER TABLE `board_posts` 
+MODIFY COLUMN `board_type` enum('CHAT', 'QUESTION', 'CREATOR', 'NONE') NOT NULL;
+
+COMMIT;
